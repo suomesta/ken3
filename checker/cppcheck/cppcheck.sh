710257d4f71cbf1260669f44276660a977e7746a
toda <shawn316michaels@gmail.com>
2018-02-11 19:20:16 +0900

    fix file path to be checked

--- checker/cppcheck/cppcheck.sh
+++ checker/cppcheck/cppcheck.sh
@@ -1,4 +1,4 @@
 cd `dirname $0`
 
-cppcheck --enable=all --suppress=unusedFunction --std=c++11 ../ken3/*.hpp ../ken3/*.cpp ../ken3/*/*.hpp ../ken3/*/*.cpp ../ken3/*/*/*.hpp ../ken3/*/*/*.cpp
+cppcheck --enable=all --suppress=unusedFunction --std=c++11 ../../ken3/*.hpp ../../ken3/*.cpp ../../ken3/*/*.hpp ../../ken3/*/*.cpp
 
