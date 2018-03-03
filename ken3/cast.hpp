b1b5c2512fadacbd300e0902b3dd2487f96b9dd4
toda <shawn316michaels@gmail.com>
2018-03-03 14:05:37 +0900

    update int_cast. now one tparam is mandatory

--- ken3/cast.hpp
+++ ken3/cast.hpp
@@ -14,23 +14,23 @@
  *     // try to convert int into short.
  *     // conversion result is OK
  *     int i = 10;
- *     short s = ken3::int_cast<int, short>(i);
+ *     short s = ken3::int_cast<short>(i);
  *     std::cout << s; // => "10"
  *
  *     // try to convert long into char.
  *     // conversion result is NG, because 70000 is too large for char
  *     long l = 70000;
  *     try {
- *         char c = ken3::int_cast<long, char>(l);
+ *         char c = ken3::int_cast<char>(l);
  *     }
  *     catch (const std::out_of_range& e) {
  *         std::cout << "out_of_range"; // => "out_of_range"
  *     }
  *
  *     // bool is not supported
- *     std::cout << ken3::int_cast<bool, int>(true); // compile error!
+ *     std::cout << ken3::int_cast<int>(true); // compile error!
  *     // parameters shall be integer
- *     std::cout << ken3::int_cast<double, int>(1.0); // compile error!
+ *     std::cout << ken3::int_cast<int>(1.0); // compile error!
  */
 
 #ifndef INCLUDE_GUARD_KEN3_CAST_HPP
@@ -46,22 +46,22 @@ namespace ken3 {
 /**
  * @function   int_cast
  * @brief      converter from integer into integer.
- * @tparam     FROM: input integer type. shall be integral.
  * @tparam     TO: output integer type. shall be integral.
+ * @tparam     FROM: input integer type. shall be integral.
  * @param[in]  i: appointed integer value.
  * @return     integer value converted from argument i. its type is TO
  * @throw      std::out_of_range: cannot keep original FROM value in TO
  */
-template <typename FROM, typename TO>
+template <typename TO, typename FROM>
 TO int_cast(FROM i)
 {
-    static_assert(std::is_integral<FROM>::value, "Integer required");
     static_assert(std::is_integral<TO>::value, "Integer required");
-    static_assert(!std::is_same<FROM, bool>::value, "int_cast() does not support bool");
+    static_assert(std::is_integral<FROM>::value, "Integer required");
     static_assert(!std::is_same<TO, bool>::value, "int_cast() does not support bool");
+    static_assert(!std::is_same<FROM, bool>::value, "int_cast() does not support bool");
 
-    using my_factory = int_cast_detail::factory<int_cast_detail::factory_index<FROM, TO>()>;
-    return int_cast_detail::int_cast<FROM, TO>(i, my_factory{});
+    using my_factory = int_cast_detail::factory<int_cast_detail::factory_index<TO, FROM>()>;
+    return int_cast_detail::int_cast<TO, FROM>(i, my_factory{});
 }
 /////////////////////////////////////////////////////////////////////////////
 
--- ken3/cast/int_cast.hpp
+++ ken3/cast/int_cast.hpp
@@ -17,50 +17,39 @@ namespace ken3 {
 namespace int_cast_detail {
 
 template <size_t S> struct factory;
-//                                   FROM      TO        Size
-template <> struct factory<0> {}; // signed,   signed,   sizeof(FROM) <= sizeof(TO)
-template <> struct factory<1> {}; // unsigned, signed,   sizeof(FROM) <= sizeof(TO)
-template <> struct factory<2> {}; // signed,   unsigned, sizeof(FROM) <= sizeof(TO)
-template <> struct factory<3> {}; // unsigned, unsigned, sizeof(FROM) <= sizeof(TO)
-template <> struct factory<4> {}; // signed,   signed,   sizeof(FROM) >  sizeof(TO)
-template <> struct factory<5> {}; // unsigned, signed,   sizeof(FROM) >  sizeof(TO)
-template <> struct factory<6> {}; // signed,   unsigned, sizeof(FROM) >  sizeof(TO)
-template <> struct factory<7> {}; // unsigned, unsigned, sizeof(FROM) >  sizeof(TO)
+//                                   TO        FROM      Size
+template <> struct factory<0> {}; // signed,   signed,   sizeof(TO) >= sizeof(FROM)
+template <> struct factory<1> {}; // unsigned, signed,   sizeof(TO) >= sizeof(FROM)
+template <> struct factory<2> {}; // signed,   unsigned, sizeof(TO) >= sizeof(FROM)
+template <> struct factory<3> {}; // unsigned, unsigned, sizeof(TO) >= sizeof(FROM)
+template <> struct factory<4> {}; // signed,   signed,   sizeof(TO) <  sizeof(FROM)
+template <> struct factory<5> {}; // unsigned, signed,   sizeof(TO) <  sizeof(FROM)
+template <> struct factory<6> {}; // signed,   unsigned, sizeof(TO) <  sizeof(FROM)
+template <> struct factory<7> {}; // unsigned, unsigned, sizeof(TO) <  sizeof(FROM)
 /////////////////////////////////////////////////////////////////////////////
 
-template <typename FROM, typename TO>
+template <typename TO, typename FROM>
 constexpr size_t factory_index(void)
 {
     return (
-        std::is_unsigned<FROM>::value +
-        std::is_unsigned<TO>::value * 2 +
+        std::is_unsigned<TO>::value +
+        std::is_unsigned<FROM>::value * 2 +
         ((sizeof(FROM) > sizeof(TO)) ? 1 : 0) * 4
     );
 }
 /////////////////////////////////////////////////////////////////////////////
 
-// signed,   signed,   sizeof(FROM) <= sizeof(To)
-template <typename FROM, typename To>
-To int_cast(FROM i, factory<0> s)
+// signed,   signed,   sizeof(TO) >= sizeof(FROM)
+template <typename TO, typename FROM>
+TO int_cast(FROM i, factory<0> s)
 {
-    return static_cast<To>(i);
-}
-/////////////////////////////////////////////////////////////////////////////
-
-// unsigned, signed,   sizeof(FROM) <= sizeof(TO)
-template <typename FROM, typename TO>
-TO int_cast(FROM i, factory<1> s)
-{
-    if (i > static_cast<FROM>(std::numeric_limits<TO>::max())) {
-        throw std::out_of_range("");
-    }
     return static_cast<TO>(i);
 }
 /////////////////////////////////////////////////////////////////////////////
 
-// signed,   unsigned, sizeof(FROM) <= sizeof(TO)
-template <typename FROM, typename TO>
-TO int_cast(FROM i, factory<2> s)
+// unsigned, signed,   sizeof(TO) >= sizeof(FROM)
+template <typename TO, typename FROM>
+TO int_cast(FROM i, factory<1> s)
 {
     if (i < 0) {
         throw std::out_of_range("");
@@ -69,16 +58,27 @@ TO int_cast(FROM i, factory<2> s)
 }
 /////////////////////////////////////////////////////////////////////////////
 
-// unsigned, unsigned, sizeof(FROM) <= sizeof(TO)
-template <typename FROM, typename TO>
+// signed,   unsigned, sizeof(TO) >= sizeof(FROM)
+template <typename TO, typename FROM>
+TO int_cast(FROM i, factory<2> s)
+{
+    if (i > static_cast<FROM>(std::numeric_limits<TO>::max())) {
+        throw std::out_of_range("");
+    }
+    return static_cast<TO>(i);
+}
+/////////////////////////////////////////////////////////////////////////////
+
+// unsigned, unsigned, sizeof(TO) >= sizeof(FROM)
+template <typename TO, typename FROM>
 TO int_cast(FROM i, factory<3> s)
 {
     return static_cast<TO>(i);
 }
 /////////////////////////////////////////////////////////////////////////////
 
-// signed,   signed,   sizeof(FROM) >  sizeof(TO)
-template <typename FROM, typename TO>
+// signed,   signed,   sizeof(TO) <  sizeof(FROM)
+template <typename TO, typename FROM>
 TO int_cast(FROM i, factory<4> s)
 {
     if ((i > static_cast<FROM>(std::numeric_limits<TO>::max())) ||
@@ -90,20 +90,9 @@ TO int_cast(FROM i, factory<4> s)
 }
 /////////////////////////////////////////////////////////////////////////////
 
-// unsigned, signed,   sizeof(From) >  sizeof(TO)
-template <typename From, typename TO>
-TO int_cast(From i, factory<5> s)
-{
-    if (i > static_cast<From>(std::numeric_limits<TO>::max())) {
-        throw std::out_of_range("");
-    }
-    return static_cast<TO>(i);
-}
-/////////////////////////////////////////////////////////////////////////////
-
-// signed,   unsigned, sizeof(FROM) >  sizeof(TO)
-template <typename FROM, typename TO>
-TO int_cast(FROM i, factory<6> s)
+// unsigned, signed,   sizeof(TO) <  sizeof(FROM)
+template <typename TO, typename FROM>
+TO int_cast(FROM i, factory<5> s)
 {
     if ((i > static_cast<FROM>(std::numeric_limits<TO>::max())) ||
         (i < 0))
@@ -114,8 +103,19 @@ TO int_cast(FROM i, factory<6> s)
 }
 /////////////////////////////////////////////////////////////////////////////
 
-// unsigned, unsigned, sizeof(FROM) >  sizeof(TO)
-template <typename FROM, typename TO>
+// signed,   unsigned, sizeof(TO) <  sizeof(FROM)
+template <typename TO, typename FROM>
+TO int_cast(FROM i, factory<6> s)
+{
+    if (i > static_cast<FROM>(std::numeric_limits<TO>::max())) {
+        throw std::out_of_range("");
+    }
+    return static_cast<TO>(i);
+}
+/////////////////////////////////////////////////////////////////////////////
+
+// unsigned, unsigned, sizeof(TO) <  sizeof(FROM)
+template <typename TO, typename FROM>
 TO int_cast(FROM i, factory<7> s)
 {
     if (i > static_cast<FROM>(std::numeric_limits<TO>::max())) {
